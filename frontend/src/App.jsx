0b4feddf1f7bab8e14de2f280a8c64f026b905b5
Pranav Rajbhandari <136177992+pranavrjb@users.noreply.github.com>
2024-12-08 20:37:44 +0545

    compnay registration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
-import Login from './pages/Login'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Services from './pages/Services'
 import Navbar from './components/Navbar'
 import AppointmentBooking from './pages/AppointmentBooking'
 import Footer from './components/Footer'
+import CompanyRegistration from './pages/CompanyRegistration'
 const App = () => {
   
   return (
@@ -18,7 +18,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/about' element={<About/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/company' element={<CompanyRegistration/>} />
         <Route path='/services' element={<Services/>} />
         <Route path='/profile' element={<Profile/>} />
         <Route path='/appointment' element={<AppointmentBooking/>}/>
