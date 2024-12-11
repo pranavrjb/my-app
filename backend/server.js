ae302f872ae7d23e94831c19e62dbf80fd3461a1
Pranav Rajbhandari <136177992+pranavrjb@users.noreply.github.com>
2024-12-11 10:39:53 +0545

    database connection

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ import mongoose from 'mongoose'
 
 const app=express()
 const port = process.env.PORT || 5000;
+const conn ="mongodb://localhost:27017/test";
 app.use(express.json())
+
+mongoose.connect(conn, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('MongooDB is connected!'))
+.catch(err=> console.log('Connection Failed!'))
 
 app.get('/',(req,res)=>{
   res.send("Welcome to appointment booking API!")
